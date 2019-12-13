e070f64c66bcd25501339bc24c45dafe25323c01
summerlin <summer.lin@logicsolutions.com>
2019-12-13 13:49:36 +0800

    finish the start&stop game buttons and its components relationships

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'binding-practice';
+  onIntervalFired(firedNumber: number){
+    console.log(firedNumber);
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Z_HUFFMAN_ONLY } from 'zlib';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-
+  @Output() intervalFired = new EventEmitter<number>(); 
+  //let polly = new Hunam();
+  interval;
+  firstNumber = 0;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame(){
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.firstNumber + 1);
+      this.firstNumber++;
+    }, 1000)
+  }
+
+  onStopGame(){
+    clearInterval(this.interval);
   }
 
 }
